f384b79bb9fe46bf97457f1a14c2fb63c05e67db
LillaDou <lilla.e.j.du@gmail.com>
2024-07-07 18:08:14 +0200

    Mostrar quote en pantalla

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -24,8 +24,22 @@ export const BreakingbadApp = async( element ) => {
 
     document.querySelector('#app-title').innerHTML = 'Breaking Bad App';
     element.innerHTML = 'Loading...';
+    // await fetchQuote();
 
-    const quote =  await fetchQuote();
-    element.innerHTML = 'Tenemos data!!';
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nexQuoteButton = document.createElement('button');
+    nexQuoteButton.innerText = 'Next Quote';
+
+    //Función para insertar los datos que hemos creado anteriormente con las variables
+    const renderQuote = ( data ) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren( quoteLabel, authorLabel, nexQuoteButton );
+    }
+
+    fetchQuote()
+        .then( renderQuote );// .then( (data) => renderQuote(data) ). Como el argumento que mandamos('data') es el mismo que pedimos también en el 
+        //RenderQuote, se puede simplificar de esta manera
 
 }
