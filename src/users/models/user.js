c27b07647e22a1e251d0c84591e8ca1ea4030ad4
LillaDou <lilla.e.j.du@gmail.com>
2024-07-10 16:46:22 +0200

    CRUD App - User Model y problemas relacionados

---
+++ src/users/models/user.js
@@ -0,0 +1,19 @@
+
+//Usuario que vamos a usar en nuestra aplicación
+export class User {
+    
+    /**
+     * 
+     * @param {Like<User>} userDataLike 
+     */
+    constructor( { id, isActive, balance, avatar, firstName, lastName, gender } ) {
+        this.id = id;
+        this.isActive = isActive; 
+        this.balance = balance;
+        this.avatar = avatar;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+}
