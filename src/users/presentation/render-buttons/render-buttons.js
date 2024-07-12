bcf8f5c6b90f523763c0bb6171d090c6a5f3e02a
LillaDou <lilla.e.j.du@gmail.com>
2024-07-12 13:07:42 +0200

    CRUD App - funcionalidad de los botones

--- src/users/presentation/render-buttons/render-buttons.js
+++ src/users/presentation/render-buttons/render-buttons.js
@@ -1,5 +1,6 @@
 import './render-buttons.css';
 import usersStore from '../../store/users-store';
+import { renderTable } from '../render-table/render-table';
 
 
 /**
@@ -22,5 +23,16 @@ export const renderButtons = ( element ) => {
     //Creamos los botones físicos para renderizar 
     element.append( prevButton, currentPageLabel, nextButton );
 
+    nextButton.addEventListener('click', async() => {
+        await usersStore.loadNextPage();
+        currentPageLabel.innerHTML = usersStore.getCurrentPage();//cual es la página actual
+        renderTable( element ); //cambiar la tabla. Pide el elemento de HTML de arriba
+    });
+
+    prevButton.addEventListener('click', async() => {
+        await usersStore.loadPreviousPage();
+        currentPageLabel.innerHTML = usersStore.getCurrentPage();
+        renderTable( element );
+    });
 
 }
