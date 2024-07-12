4bdd0b5302da53de0b02d84ec29e20dbfdce8376
LillaDou <lilla.e.j.du@gmail.com>
2024-07-12 14:10:14 +0200

    CRUD App - comportamiento esperado del modal

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -4,8 +4,17 @@ import './render-modal.css';
 import modalHtml from './render-modal.html?raw';
 //En vite se añade el ?raw al html para que pueda ser leído
 
-let modal;
+let modal, form;
 
+//TODO: cargar usuario por id
+export const showModal = () => {
+    modal?.classList.remove('hide-modal'); //Si existe el modal, quítalo
+
+}
+
+export const hideModal = () => {
+    modal?.classList.add('hide-modal');//Si existe el modal, añade el hide-modal
+}
 
 /**
  * 
@@ -18,6 +27,20 @@ export const renderModal = ( element ) => {
     modal = document.createElement('div');
     modal.innerHTML = modalHtml;
     modal.className = 'modal-container hide-modal';
+    form = modal.querySelector('form');
+
+    modal.addEventListener('click', (event) => {
+    //Si el lugar donde hacemos click es exactamente igual al modal-container, cierra el modal
+        if ( event.target.className === 'modal-container' ) {
+            hideModal();
+        }
+    });
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        console.log('Formulario enviado');
+    });
 
     element.append( modal );
 }
